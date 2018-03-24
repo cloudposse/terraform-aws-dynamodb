45cb3dc63dc24a37900752a9fae5d89291f41611
Andriy Knysh <aknysh@users.noreply.github.com>
2018-03-24 18:01:04 -0400

    Use `terraform-aws-dynamodb-autoscaler` module (#5)
    * Use `terraform-aws-dynamodb-autoscaler` module
    
    * Add `enable_autoscaler` variable

--- variables.tf
+++ variables.tf
@@ -1,67 +1,90 @@
-variable "name" {
-  type = "string"
-}
-
 variable "namespace" {
-  type = "string"
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "stage" {
-  type = "string"
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
 }
 
-variable "enable_encryption" {
-  default = "true"
-}
-
-variable "attributes" {
-  type    = "list"
-  default = []
-}
-
-variable "tags" {
-  type    = "map"
-  default = {}
+variable "name" {
+  type        = "string"
+  description = "Name  (e.g. `app` or `cluster`)"
 }
 
 variable "delimiter" {
-  type    = "string"
-  default = "-"
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
 }
 
-variable "hash_key" {
-  type = "string"
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
-variable "range_key" {
-  type = "string"
-}
-
-variable "ttl_attribute" {
-  type    = "string"
-  default = "Expires"
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
 }
 
 variable "autoscale_write_target" {
-  default = 50
+  default     = 10
+  description = "The target value for DynamoDB write autoscaling"
 }
 
 variable "autoscale_read_target" {
-  default = 50
+  default     = 10
+  description = "The target value for DynamoDB read autoscaling"
 }
 
 variable "autoscale_min_read_capacity" {
-  default = 10
+  default     = 5
+  description = "DynamoDB autoscaling min read capacity"
 }
 
 variable "autoscale_max_read_capacity" {
-  default = 100
+  default     = 20
+  description = "DynamoDB autoscaling max read capacity"
 }
 
 variable "autoscale_min_write_capacity" {
-  default = 10
+  default     = 5
+  description = "DynamoDB autoscaling min write capacity"
 }
 
 variable "autoscale_max_write_capacity" {
-  default = 100
+  default     = 20
+  description = "DynamoDB autoscaling max write capacity"
+}
+
+variable "enable_encryption" {
+  type        = "string"
+  default     = "true"
+  description = "Enable DynamoDB server-side encryption"
+}
+
+variable "hash_key" {
+  type        = "string"
+  description = "DynamoDB table Hash Key"
+}
+
+variable "range_key" {
+  type        = "string"
+  description = "DynamoDB table Range Key"
+}
+
+variable "ttl_attribute" {
+  type        = "string"
+  default     = "Expires"
+  description = "DynamoDB table TTL attribute"
+}
+
+variable "enable_autoscaler" {
+  type        = "string"
+  default     = "true"
+  description = "Flag to enable/disable DynamoDB autoscaling"
 }
