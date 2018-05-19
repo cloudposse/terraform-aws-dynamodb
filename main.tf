e329d72cfde4a2455d3a0b1acc768579f325013a
Jamie Nelson <jamie@theinstillery.com>
2018-05-19 14:59:28 +0100

    switched to terraform-terraform-label for modern sleekness

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "dynamodb_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.2"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
