0b661c19c1f974d25d1d97f300ee12a9cf48e4d6
Jacek <jacek@ivolution.pl>
2019-04-16 13:18:47 +0200

    var

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_dynamodb_table" "default" {
 
   ttl {
     attribute_name = "${var.ttl_attribute}"
-    enabled        = "${length(ttl_attribute) > 0}"
+    enabled        = "${length(var.ttl_attribute) > 0}"
   }
 
   tags = "${module.dynamodb_label.tags}"
