3ea9491c49f1fa29e7d56b9409aeb33f84b38682
Jürgen Weber <jurgen@space-marine.org>
2020-12-03 11:13:32 +1100

    fix up label

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "dynamodb_label" {
-  source              = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
+  source              = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.21.0"
   enabled             = var.enabled
   namespace           = var.namespace
   stage               = var.stage
