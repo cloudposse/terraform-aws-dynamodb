7a91ff5f8801f1328ec70f001f1e36957f279bc6
Michael Warkentin <mwarkentin@hey.com>
2021-04-28 16:14:19 -0400

    Configure stream settings if var.replicas set

--- main.tf
+++ main.tf
@@ -51,8 +51,9 @@ resource "aws_dynamodb_table" "default" {
   write_capacity   = var.autoscale_min_write_capacity
   hash_key         = var.hash_key
   range_key        = var.range_key
-  stream_enabled   = var.enable_streams
-  stream_view_type = var.enable_streams ? var.stream_view_type : ""
+  stream_enabled   = length(var.replicas) > 0 ? true : var.stream_enabled
+  stream_view_type = length(var.replicas) > 0 || var.enable_streams ? var.stream_view_type : ""
+
 
   server_side_encryption {
     enabled     = var.enable_encryption
