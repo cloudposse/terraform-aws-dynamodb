d9948ad65e0e24467768a8768f79fe908638abcd
Maxim Mironenko <maxim@cloudposse.com>
2021-05-20 16:42:09 +0600

    update autoscaler version

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,29 +1,59 @@
-output "table_name" {
-  value       = module.dynamodb_table.table_name
+output "table_name_1" {
+  value       = module.dynamodb_table_1.table_name
   description = "DynamoDB table name"
 }
 
-output "table_id" {
-  value       = module.dynamodb_table.table_id
+output "table_id_1" {
+  value       = module.dynamodb_table_1.table_id
   description = "DynamoDB table ID"
 }
 
-output "table_arn" {
-  value       = module.dynamodb_table.table_arn
+output "table_arn_1" {
+  value       = module.dynamodb_table_1.table_arn
   description = "DynamoDB table ARN"
 }
 
-output "global_secondary_index_names" {
-  value       = module.dynamodb_table.global_secondary_index_names
+output "global_secondary_index_names_1" {
+  value       = module.dynamodb_table_1.global_secondary_index_names
   description = "DynamoDB secondary index names"
 }
 
-output "table_stream_arn" {
-  value       = module.dynamodb_table.table_stream_arn
+output "table_stream_arn_1" {
+  value       = module.dynamodb_table_1.table_stream_arn
   description = "DynamoDB table stream ARN"
 }
 
-output "table_stream_label" {
-  value       = module.dynamodb_table.table_stream_label
+output "table_stream_label_1" {
+  value       = module.dynamodb_table_1.table_stream_label
+  description = "DynamoDB table stream label"
+}
+
+output "table_name_2" {
+  value       = module.dynamodb_table_2.table_name
+  description = "DynamoDB table name"
+}
+
+output "table_id_2" {
+  value       = module.dynamodb_table_2.table_id
+  description = "DynamoDB table ID"
+}
+
+output "table_arn_2" {
+  value       = module.dynamodb_table_2.table_arn
+  description = "DynamoDB table ARN"
+}
+
+output "global_secondary_index_names_2" {
+  value       = module.dynamodb_table_2.global_secondary_index_names
+  description = "DynamoDB secondary index names"
+}
+
+output "table_stream_arn_2" {
+  value       = module.dynamodb_table_2.table_stream_arn
+  description = "DynamoDB table stream ARN"
+}
+
+output "table_stream_label_2" {
+  value       = module.dynamodb_table_2.table_stream_label
   description = "DynamoDB table stream label"
 }
--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -22,16 +22,30 @@ func TestExamplesComplete(t *testing.T) {
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
+	// This will run `terraform init` and `terraform plan` and fail the test if there are any errors
+	terraform.InitAndPlan(t, terraformOptions)
+
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of an output variable
-	tableName := terraform.Output(t, terraformOptions, "table_name")
+	tableName1 := terraform.Output(t, terraformOptions, "table_name_1")
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, "eg-test-dynamodb-table", tableName)
+	assert.Equal(t, "eg-test-first", tableName1)
 
 	// Run `terraform output` to get the value of an output variable
-	tableArn := terraform.Output(t, terraformOptions, "table_arn")
+	tableArn1 := terraform.Output(t, terraformOptions, "table_arn_1")
 	// Verify we're getting back the outputs we expect
-	assert.Contains(t, tableArn, "table/eg-test-dynamodb-table")
+	assert.Contains(t, tableArn1, "table/eg-test-first")
+
+	// Run `terraform output` to get the value of an output variable
+	tableName2 := terraform.Output(t, terraformOptions, "table_name_2")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "eg-test-second", tableName2)
+
+	// Run `terraform output` to get the value of an output variable
+	tableArn2 := terraform.Output(t, terraformOptions, "table_arn_2")
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, tableArn2, "table/eg-test-second")
+
 }
