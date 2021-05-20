a8729401c159af8c1aa7ed628785f5957a121068
Michael Warkentin <mwarkentin@hey.com>
2021-05-20 14:36:15 -0400

    Use count on autoscaler module to disable
    after https://github.com/cloudposse/terraform-aws-dynamodb-autoscaler/pull/37 was merged, some autoscaler resources were automatically created, even if enable_autoscaler = false
    
    Closes #92

--- main.tf
+++ main.tf
@@ -120,6 +120,9 @@ resource "aws_dynamodb_table" "default" {
 module "dynamodb_autoscaler" {
   source  = "cloudposse/dynamodb-autoscaler/aws"
   version = "0.13.0"
+
+  count = local.enabled && var.enable_autoscaler && var.billing_mode == "PROVISIONED" ? 1 : 0
+
   enabled = local.enabled && var.enable_autoscaler && var.billing_mode == "PROVISIONED"
 
   attributes                   = concat(module.this.attributes, var.autoscaler_attributes)
