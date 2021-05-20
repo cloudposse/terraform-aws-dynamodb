2e7e68f0869724df201f78be8da2db45a6daa972
Maxim Mironenko <maxim@cloudposse.com>
2021-05-20 16:49:58 +0600

    update autoscaler version (#87)
    * update autoscaler version
    
    * readme updated
    
    * Auto Format
    
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,12 +2,66 @@ provider "aws" {
   region = var.region
 }
 
-module "dynamodb_table" {
+module "dynamodb_table_1" {
   source = "../../"
 
+  name              = "first"
   hash_key          = "HashKey"
   range_key         = "RangeKey"
-  enable_autoscaler = false
+  enable_autoscaler = true
+
+  dynamodb_attributes = [
+    {
+      name = "DailyAverage"
+      type = "N"
+    },
+    {
+      name = "HighWater"
+      type = "N"
+    },
+    {
+      name = "Timestamp"
+      type = "S"
+    }
+  ]
+
+  local_secondary_index_map = [
+    {
+      name               = "TimestampSortIndex"
+      range_key          = "Timestamp"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["HashKey", "RangeKey"]
+    },
+    {
+      name               = "HighWaterIndex"
+      range_key          = "Timestamp"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["HashKey", "RangeKey"]
+    }
+  ]
+
+  global_secondary_index_map = [
+    {
+      name               = "DailyAverageIndex"
+      hash_key           = "DailyAverage"
+      range_key          = "HighWater"
+      write_capacity     = 5
+      read_capacity      = 5
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["HashKey", "RangeKey"]
+    }
+  ]
+
+  context = module.this.context
+}
+
+module "dynamodb_table_2" {
+  source = "../../"
+
+  name              = "second"
+  hash_key          = "HashKey"
+  range_key         = "RangeKey"
+  enable_autoscaler = true
 
   dynamodb_attributes = [
     {
